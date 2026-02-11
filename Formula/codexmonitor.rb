b61cccea341f4210ef498d96a09dab9bd4f7ce3a
Oliver Drobnik <oliver@cocoanetics.com>
2026-02-11 18:42:02 +0100

    codexmonitor 0.1.1

--- Formula/codexmonitor.rb
+++ Formula/codexmonitor.rb
@@ -1,8 +1,8 @@
 class Codexmonitor < Formula
   desc "List, inspect, and watch local OpenAI Codex sessions"
   homepage "https://github.com/Cocoanetics/CodexMonitor"
-  url "https://github.com/Cocoanetics/CodexMonitor/archive/refs/tags/0.1.0.tar.gz"
-  sha256 "25cf4eb16a3146e35b1bfde6fc5df534884161f4f768c995c48b84eac59951bf"
+  url "https://github.com/Cocoanetics/CodexMonitor/archive/refs/tags/0.1.1.tar.gz"
+  sha256 "1e5ead48e8a6c7eebbd22525c6769860396d5d2b1d8912794504a06dd4dc8aa7"
   license "MIT"
 
   depends_on xcode: ["16.0", :build]
@@ -15,6 +15,6 @@ class Codexmonitor < Formula
   end
 
   test do
-    system "#{bin}/codexmonitor", "--help"
+    system bin/"codexmonitor", "--help"
   end
 end
