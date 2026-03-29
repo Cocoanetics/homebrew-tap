99fc4d388768c77f442b675416f7e1b0ffaff3be
Oliver Drobnik <oliver@cocoanetics.com>
2026-03-29 19:38:02 +0200

    swiftbutler 1.0.0

---
+++ Formula/swiftbutler.rb
@@ -0,0 +1,30 @@
+class Swiftbutler < Formula
+  desc "Swift source analysis, syntax checking, reindentation, and file distribution"
+  homepage "https://github.com/Cocoanetics/SwiftButler"
+  url "https://github.com/Cocoanetics/SwiftButler/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "4932e801fe4f99a8379421cffba9b360ff169714e180ba029d289d86b0eae57f"
+  license "MIT"
+
+  on_macos do
+    depends_on xcode: ["16.0", :build]
+  end
+
+  on_linux do
+    depends_on "swift" => :build
+  end
+
+  def install
+    # Write the formula version so the SwiftPM build plugin can pick it up
+    # (since the Homebrew tarball has no git history for `git describe`)
+    File.write(".version", version)
+
+    # SwiftPM plugins + Homebrew sandboxing can conflict on some systems.
+    # Disable SwiftPM sandbox to ensure the manifest and build steps can run.
+    system "swift", "build", "-c", "release", "--disable-sandbox"
+    bin.install ".build/release/butler"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/butler --version")
+  end
+end
